30492265bfb047095e3e347fb1653c52ce1f0c6d
Alice <133613280+alicegms@users.noreply.github.com>
2023-09-06 14:01:38 -0300

    Lista 2
    Continuação dos exercícios da lista 2 de programação orientada à objeto.

---
+++ Lista2 Exercicio 7/Lista2 Exercicio 7/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_Exercicio_7
+{
+    internal class Pessoa
+    {
+        //constructor para adicionar sempre adicionar a quantidade de pessoas novas
+        public Pessoa()
+        {
+            Pessoa.TotalPessoas++;
+        }
+        public static int TotalPessoas { get; private set; }
+
+    }
+}
---
+++ Lista2 Exercicio 7/Lista2 Exercicio 7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_Exercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Construa uma classe chamada Pessoa em C# com uma propriedade estática
+            chamada TotalPessoas que guarda o número total de instâncias da classe criadas.
+            O valor de TotalPessoas deve ser atualizado sempre que uma nova instância de
+            Pessoa for criada.*/
+
+
+            Pessoa v = new Pessoa();
+            Pessoa v2 = new Pessoa();
+            Pessoa v3 = new Pessoa();
+            Console.WriteLine("Total de pessoas: " + Pessoa.TotalPessoas);
+
+            Console.ReadKey();
+        }
+    }
+}
